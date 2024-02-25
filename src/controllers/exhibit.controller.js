5afd66fcac87c603a81a13eb522ce86f5e046064
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-02-25 01:44:18 +0100

    its possible to request resources through a list of multiple ids now, including duplicates and null elements

--- src/controllers/exhibit.controller.js
+++ src/controllers/exhibit.controller.js
@@ -37,14 +37,35 @@ export const getExhibitByIds = async (req,res) =>
     // Split by Ids:
     const ids = req.params.exhibitIds.split("&")
     console.log(ids)
+
+    // Filter out the 'nulls':
+
+    const existing_ids = ids.filter((id) => id != "null")
+    console.log(existing_ids)
         
     // Obtain list of elements:
-    const exhibits = await Exhibit.find({ '_id': { $in: ids } });
+    const unique_exhibits = await Exhibit.find({ '_id': { $in: existing_ids } });
+    //console.log(unique_exhibits)
+
+    // Recreate list of elements including the repeats:
+
+    const buffer = {}
+    unique_exhibits.forEach(o => buffer[o._id] = o)
+    const exhibits = ids.map(id => buffer[id] ? buffer [id] : null)
+
+    // Recreate list of elements including the nulls:
+    
+
+
+    console.log(exhibits)
+
     res.status(200).json(exhibits)
 }
 
 export const updateExhibitById = async (req,res) => 
 {
+    // we use this same method to update a single element in the model slots list
+    // 
     const updatedExhibit = await Exhibit.findByIdAndUpdate(req.params.exhibitId,req.body,{new:true})
     res.status(200).json(updatedExhibit)
 
