bd8df85f90d53c1f7af4f370f6da99c57fbb01a0
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-15 04:04:51 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 //app.js configura la aplicación de express
 
-//test commit
+//testing the automatic deployment lets go
 
 import express from 'express'
 import morgan from 'morgan' //middleware de express que nos muestra las peticiones que llegan a nuestro navegador
