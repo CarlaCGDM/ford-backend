919a7a58c0cf701d39da2db618229db11d08a885
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-18 14:56:33 +0200

    trying again to fix cors error

--- src/app.js
+++ src/app.js
@@ -22,13 +22,7 @@ app.set('pkg', pkg) //guardar el valor de esta variable
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors())
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.options('*', cors());
 
 app.get('/', (req,res) => {
     res.json({
