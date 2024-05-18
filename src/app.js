2052e58923e7b163145f8a1c578e80346773c3c8
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-18 16:07:00 +0200

    fixing cors error

--- src/app.js
+++ src/app.js
@@ -21,8 +21,8 @@ app.set('pkg', pkg) //guardar el valor de esta variable
 
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(cors())
 app.options('*', cors());
+app.use(cors())
 
 app.get('/', (req,res) => {
     res.json({
